39ee0d93431049855d41763c40932855d1eba414
yqq <yqq100822@live.com>
2018-03-19 18:43:55 +0800

    reverse integer001

---
+++ reverse integer.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverse(int x) {
+        if (!x|| x==INT_MIN) return 0;
+    if (x < 0) return -1 * reverse(-x);
+    while (!(x % 10)) x /= 10;
+    long long  ans=0;
+    while (x) ans = ans * 10 + x % 10, x /= 10;
+    return (ans>INT_MAX ||ans<INT_MIN)?0:ans;
+    }
+};
