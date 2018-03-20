ef7ed5bdb0989ba0578c69360ce5cb5ffef36ff0
ScorpioY <770131286@qq.com>
2018-03-20 19:58:21 +0800

    Palindrome Number001
    回文数

---
+++ Palindrome Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)  return false;
+        if (x < 10)  return true;
+        int tmp = x;
+        int len = 0;
+        while(tmp){
+            tmp /= 10, ++len;
+        }
+        int left = pow(10, len - 1);
+        int right = 1;
+        while(left > right){
+            if (x / left % 10 != x / right % 10)  return false;
+            left /= 10;
+            right *= 10;
+        }
+        return true;
+    }
+};
